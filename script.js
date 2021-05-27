4ac48ba203e2d7af6077a7c996ff94b07481edca
Geoff Ostman <81042162+geoff-ostman@users.noreply.github.com>
2021-05-27 16:32:19 -0500

    final push

--- script.js
+++ script.js
@@ -1,50 +1,56 @@
-const getLocation = async() => {
+const getLocation = async () => {
     const response = await fetch()
-  
+
     if (response.status === 200) {
-      const data = await response.json()
-      return data.city
+        const data = await response.json()
+        return data.city
     } else {
-      throw new Error('Unable to get the current location')
+        throw new Error('Unable to get the current location')
     }
-  }
-  
-  const getWeather = async(currentCity) => {
+}
+
+const getWeather = async (currentCity) => {
     const response = await
-    fetch("http://api.openweathermap.org/data/2.5/weather?id={city id}&appid={7ac6704f97ab9101ae66ec2d6fc627d8}");
-  
+        fetch("http://api.openweathermap.org/data/2.5/weather?id={city id}&appid={7ac6704f97ab9101ae66ec2d6fc627d8}");
+
     if (response.status === 200) {
-      const data = await response.json()
-      return data
+        const data = await response.json()
+        return data
     } else {
-      throw new Error("Unable to get weather")
+        throw new Error("Unable to get weather")
     }
-  }
-  
-  getLocation().then((city) => {
+}
+
+getLocation().then((city) => {
     currentCity = city;
     document.write(currentCity);
     return city;
-  }).then((city) => {
+}).then((city) => {
     return getWeather(city);
-  }).then((data) => {
+}).then((data) => {
     console.log(data)
-  }).catch((err) => {
-  })
+}).catch((err) => {
+})
 
-const r_text = new Array ();
-    r_text[0] = "meatballs";
-    r_text[1] = "raining cats and dogs";
-    r_text[2] = "raining men";
-    r_text[3] = "silver linings";
-    r_text[4] = "fair weather friends";
-    r_text[5] = "throwing caution to the wind";
-    r_text[6] = "chasing rainbows";
+function city()
+{
+    console.log(text);
+}
 
-    const i = Math.floor(7 * Math.random())
+function search()
+{
+    console.log("click");
+}
 
-    document.write(r_text[i]);
+const r_text = new Array();
+r_text[0] = "meatballs";
+r_text[1] = "raining cats and dogs";
+r_text[2] = "raining men";
+r_text[3] = "silver linings";
+r_text[4] = "fair weather friends";
+r_text[5] = "throwing caution to the wind";
+r_text[6] = "chasing rainbows";
 
-document.getElementById.searchBtn = addEventListener('click',function(weatherAPI) {
+const i = Math.floor(7 * Math.random())
 
-});
+document.write(r_text[i]);
